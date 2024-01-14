ca488e919e0dbb84f8c4bd78223bc52ec8babccd
Akinrinola Muiz Ayomide <125732933+MuizAyomide@users.noreply.github.com>
2024-01-14 22:33:49 +0000

    Create orders.js

---
+++ orders.js
@@ -0,0 +1,26 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '86787',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Akinrinola WI-FI Drone',
+        productNumber: '86787',
+        paymentStatus: 'Paid',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '86787',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName: 'Foldable Micro Drone',
+        productNumber: '86787',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+]
